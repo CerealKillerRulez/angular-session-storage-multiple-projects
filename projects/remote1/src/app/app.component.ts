6c83ec9e2653efda1a94dec77b1578d26ce88fbf
Michele Panipucci <michele.panipucci@timeware.it>
2022-09-08 15:52:27 +0200

    cross domain storage

--- projects/remote1/src/app/app.component.ts
+++ projects/remote1/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { fromEvent, Subscription } from 'rxjs';
+import { CrossDomainStorageService } from 'uikitlibrary';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit, OnDestroy {
   title = 'remote1';
+  private subs = new Subscription();
+  
+  constructor(private crossDomain: CrossDomainStorageService) {
+
+  }
+
+  public ngOnInit(): void {
+    this.crossDomain.setContainerWindow(window);
+    this.crossDomain.startListeningFromHost();
+  }
+
+  public ngOnDestroy(): void {
+    this.subs.unsubscribe();
+  }
 }
+
